7efeb110b716228ee3dbd15f26a6f432ce8f1fd7
shahriaarrr <shahriaarrr@gmail.com>
2022-06-09 10:02:30 +0430

    add our python code

---
+++ src/with_match_case.py
@@ -0,0 +1,19 @@
+number = int(input("Enter your number Between 1 and 7: "))
+
+match number:
+    case 1:
+        print("shanbe")
+    case 2:
+        print("yekshanbe")
+    case 3:
+        print("doshanbe")
+    case 4:
+        print("seshanbe")
+    case 5:
+        print("charshanbe")
+    case 6:
+        print("pangshanbe")
+    case 7:
+        print("jome")
+    case _:
+        print("Error")
---
+++ src/without_match_case.py
@@ -0,0 +1,18 @@
+number = int(input("Enter your number Between 1 and 7: "))
+
+if number == 1:
+    print("shanbe")
+elif number == 2:
+    print("yekshanbe")
+elif number == 3:
+    print("Doshanbe")
+elif number == 4:
+    print("seshanbe")
+elif number == 5:
+    print("charshanbe")
+elif number == 6:
+    print("pangshanbe")
+elif number == 7:
+    print("jomee")
+else:
+    print("Error")
